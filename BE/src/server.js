302a56aa2576caa1cbc4da48ed6c02f3ecac6cee
Amit Kumar <85306285+amitlvs@users.noreply.github.com>
2022-10-05 20:36:50 +0530

    A-Kart : BE builder code added

---
+++ BE/src/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const server = express();
+
+server.listen(8080);
+console.log("Listening to port 8080");
